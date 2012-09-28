f08b930b6f48eac473f5f52fa5ca88a62026efcd
Matthew Casperson <mcaspers@redhat.com>
2012-09-28 15:15:24 +1000

    initial import

---
+++ script.js
@@ -0,0 +1,36 @@
+const oneDay=1000*60*60*24;
+const filename="/Backup/sql/ecs_cloud_mysql.sql.gz.gpg"
+
+const client = new Dropbox.Client({
+		key: "omRCGRVEY3A=|puIE7RBXBLNdDzYvWcojIONWT3loQP1rIbny+uaurg==", sandbox: false
+});
+
+client.authDriver(new Dropbox.Drivers.Redirect());
+
+function checkBackup(client)
+{
+	client.stat(filename, function(error, stat, entries) {
+		var modified = stat.modifiedAt;
+		var now = new Date();
+		var difference = now.getTime() - modified.getTime();
+		
+		document.getElementById("filename").innerHTML = filename;
+		document.getElementById("modified").innerHTML = modified.toString();
+		var messageResult = document.getElementById("result");
+		
+		if (difference > oneDay)
+			messageResult.innerHTML = "Check your backups!";
+		else
+			messageResult.innerHTML = "All is OK.";
+	});
+}
+
+client.authenticate(function(error, client) {
+  if (error) {
+    return alert(error);
+  }
+
+  checkBackup(client);
+});
+
+
