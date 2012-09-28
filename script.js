4fe279dc83722862049dede7965ca623050191c7
Matthew Casperson <mcaspers@redhat.com>
2012-09-28 16:15:26 +1000

    Small fixes

--- script.js
+++ script.js
@@ -13,61 +13,50 @@ function checkBackup(client)
 	
 	for (var i = 0; i < filenames.length; ++i)
 	{
-		var myIndex = i;
-		
-		client.stat(filenames[myIndex], function(error, stat, entries) {
-			var modified = stat.modifiedAt;
-			var now = new Date();
-			var difference = now.getTime() - modified.getTime();
-			var ok = difference < oneDay;
-			
-			var fileResultsElement = document.getElementById("fileResults");
-			if (fileResultsElement != null)
-			{
-				fileResultsElement.innerHTML += "<tr><td>Filename:</td><td><div id='filename" + myIndex + "'></div></td></tr>\n";
-				fileResultsElement.innerHTML += "<tr><td>Modified:</td><td><div id='modified" + myIndex + "'></div></td></tr>\n";
-				fileResultsElement.innerHTML += "<tr><td>Modified Days Ago:</td><td><div id='modifiedDays" + myIndex + "'></div></td></tr>\n";
-			}
-			
-			var filenameElement = document.getElementById("filename" + myIndex);
-			if (filenameElement != null)
-				filenameElement.innerHTML = filenames[myIndex];
-			
-			var modifiedElement = document.getElementById("modified" + myIndex);
-			if (modifiedElement != null)
-				modifiedElement.innerHTML = modified.toString();
-			
-			var modifiedDays = document.getElementById("modifiedDays" + myIndex);
-			if (modifiedDays != null)
-				modifiedDays.innerHTML = difference / oneDay;
-			
-			if (ok)
-			{
-				messageResult.innerHTML = "All is OK.";
-				filenameElement.className = modifiedElement.className = modifiedDays.className = "okFile";
-			}
-			else
-			{
-				messageResult.innerHTML = "Check your backups!";
-				filenameElement.className = modifiedElement.className = modifiedDays.className = "badFile";
-				allOk = false;
-			}
-			
-		});
+		checkFilecheckFile(i)	
 	}
+}
+
+function checkFile(myIndex)
+{
+	client.stat(filenames[myIndex], function(error, stat, entries) {
+		var modified = stat.modifiedAt;
+		var now = new Date();
+		var difference = now.getTime() - modified.getTime();
+		var ok = difference < oneDay;
 		
-		var messageResult = document.getElementById("result");
-	
-		if (allOk)
+		var fileResultsElement = document.getElementById("fileResults");
+		if (fileResultsElement != null)
+		{
+			fileResultsElement.innerHTML += "<tr><td>Filename:</td><td><div id='filename" + myIndex + "'></div></td></tr>\n";
+			fileResultsElement.innerHTML += "<tr><td>Modified:</td><td><div id='modified" + myIndex + "'></div></td></tr>\n";
+			fileResultsElement.innerHTML += "<tr><td>Modified Days Ago:</td><td><div id='modifiedDays" + myIndex + "'></div></td></tr>\n";
+		}
+		
+		var filenameElement = document.getElementById("filename" + myIndex);
+		if (filenameElement != null)
+			filenameElement.innerHTML = filenames[myIndex];
+		
+		var modifiedElement = document.getElementById("modified" + myIndex);
+		if (modifiedElement != null)
+			modifiedElement.innerHTML = modified.toString();
+		
+		var modifiedDays = document.getElementById("modifiedDays" + myIndex);
+		if (modifiedDays != null)
+			modifiedDays.innerHTML = difference / oneDay;
+		
+		if (ok)
 		{
 			messageResult.innerHTML = "All is OK.";
+			filenameElement.className = modifiedElement.className = modifiedDays.className = "okFile";
 		}
 		else
 		{
 			messageResult.innerHTML = "Check your backups!";
+			filenameElement.className = modifiedElement.className = modifiedDays.className = "badFile";
 		}
-			
-	
+		
+	});
 }
 
 client.authenticate(function(error, client) {
